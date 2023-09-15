34b2d5862899219236be44b919af0e446707ecef
Paulo Ricardo M <pr741234@gmail.com>
2023-09-14 22:36:42 -0300

    :recycle: refactor: Refatoração index.js

--- src/js/index.js
+++ src/js/index.js
@@ -8,29 +8,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
   const todos = [];
 
-  function addTodoItem(item) {
-    const todo = new Todo(item);
-    todos.push(todo);
-    renderTodos();
-  }
-
-  function removeTodoItem(index) {
-    todos.splice(index, 1);
-    renderTodos();
-  }
-
-  function editTodoItem(index) {
-    const input = document.querySelector(`[data-index="${index}"]`);
-    const newValue = input.value;
-    todos[index].item = newValue;
-    renderTodos();
-  }
-
-  function toggleTodoItem(index) {
-    todos[index].isCompleted = !todos[index].isCompleted;
-    // renderTodos();
-  }
-
   const toggleOpacity = (element, opacity) => {
     element.style.opacity = opacity;
   };
@@ -39,122 +16,130 @@ document.addEventListener("DOMContentLoaded", function () {
     element.style.display = display;
   };
 
-  // Função para renderizar as tarefas na página
-  function renderTodos() {
+  const getItemInput = (index) => {
+    return document.querySelector(`[data-index="${index}"]`);
+  };
+
+  const updateItemCount = () => {
+    const itemsLeft = document.getElementById("items-left");
+    const activeTodos = todos.filter((todo) => !todo.isCompleted);
+    itemsLeft.textContent = activeTodos.length;
+  };
+
+  const renderTodoItem = (todo, index) => {
+    const todoItem = document.createElement("li");
+    todoItem.classList.add("card", "list_tasks");
+
+    const taskContent = document.createElement("div");
+    taskContent.classList.add("task_content");
+
+    const input = document.createElement("input");
+    input.setAttribute("type", "text");
+    input.setAttribute("disabled", "disabled");
+    input.classList.add("task_input");
+    input.setAttribute("name", "name");
+    input.setAttribute("autocomplete", "none");
+    input.setAttribute("data-index", index);
+    input.value = todo.item;
+
+    const actionsTasks = document.createElement("div");
+    actionsTasks.classList.add("actions_tasks");
+
+    const checkButton = document.createElement("button");
+    checkButton.classList.add("task_button", "button_check");
+
+    const selectTaskButton = document.createElement("i");
+    selectTaskButton.classList.add("fa-regular", "fa-circle");
+    selectTaskButton.id = "circle_task";
+    selectTaskButton.addEventListener("click", () => {
+      toggleOpacity(selectTaskButton, 0);
+      toggleDisplay(checkTaskButton, "flex");
+      toggleOpacity(checkTaskButton, 1);
+      input.classList.toggle("check");
+      toggleTodoItem(index);
+      updateItemCount();
+    });
+
+    const checkTaskButton = document.createElement("i");
+    checkTaskButton.classList.add("fa-regular", "fa-circle-check");
+    checkTaskButton.id = "circle_check_task";
+    checkTaskButton.addEventListener("click", () => {
+      toggleOpacity(selectTaskButton, 1);
+      toggleDisplay(checkTaskButton, "none");
+      toggleOpacity(checkTaskButton, 0);
+      input.classList.toggle("check");
+      toggleTodoItem(index);
+      updateItemCount();
+    });
+
+    const editDeleteButton = document.createElement("button");
+    editDeleteButton.classList.add("task_button", "edit_delete_button");
+
+    const editTaskButton = document.createElement("i");
+    editTaskButton.classList.add("fa-regular", "fa-pen-to-square");
+    editTaskButton.id = "edit_task";
+    editTaskButton.addEventListener("click", () => {
+      toggleOpacity(editTaskButton, 0);
+      toggleDisplay(saveTaskButton, "flex");
+      toggleOpacity(saveTaskButton, 1);
+      input.removeAttribute("disabled");
+      input.focus();
+    });
+
+    const saveTaskButton = document.createElement("i");
+    saveTaskButton.classList.add("fa-solid", "fa-check");
+    saveTaskButton.id = "save_task";
+    saveTaskButton.addEventListener("click", () => {
+      const updatedIndex = input.getAttribute("data-index");
+      editTodoItem(updatedIndex);
+      toggleOpacity(saveTaskButton, 0);
+      toggleOpacity(editTaskButton, 1);
+      toggleDisplay(saveTaskButton, "none");
+      input.setAttribute("disabled", "disabled");
+    });
+
+    const deleteTaskButton = document.createElement("i");
+    deleteTaskButton.classList.add("fa-regular", "fa-trash-can");
+    deleteTaskButton.id = "delete_task";
+    deleteTaskButton.addEventListener("click", () => {
+      removeTodoItem(index);
+    });
+
+    checkButton.appendChild(selectTaskButton);
+    checkButton.appendChild(checkTaskButton);
+
+    editDeleteButton.appendChild(editTaskButton);
+    editDeleteButton.appendChild(deleteTaskButton);
+    editDeleteButton.appendChild(saveTaskButton);
+
+    taskContent.appendChild(input);
+
+    actionsTasks.appendChild(checkButton);
+    actionsTasks.appendChild(editDeleteButton);
+
+    todoItem.appendChild(taskContent);
+    todoItem.appendChild(actionsTasks);
+
+    if (todo.isCompleted) {
+      input.classList.toggle("check");
+      toggleOpacity(selectTaskButton, 0);
+      toggleDisplay(checkTaskButton, "flex");
+      toggleOpacity(checkTaskButton, 1);
+    }
+
+    return todoItem;
+  };
+
+  // Renderiza as tarefas na página
+  const renderTodos = () => {
     const todosContainer = document.querySelector(".todos");
     todosContainer.innerHTML = "";
 
     todos.forEach((todo, index) => {
-      const todoItem = document.createElement("li");
-      todoItem.classList.add("card", "list_tasks");
-
-      const taskContent = document.createElement("div");
-      taskContent.classList.add("task_content");
-
-      const input = document.createElement("input");
-      input.setAttribute("type", "text");
-      input.setAttribute("disabled", "disabled");
-      input.classList.add("task_input");
-      input.setAttribute("name", "name");
-      input.setAttribute("autocomplete", "none");
-      input.setAttribute("data-index", index);
-      input.value = todo.item;
-
-      const actionsTasks = document.createElement("div");
-      actionsTasks.classList.add("actions_tasks");
-
-      const checkButton = document.createElement("button");
-      checkButton.classList.add("task_button", "button_check");
-
-      const selectTaskButton = document.createElement("i");
-      selectTaskButton.classList.add("fa-regular", "fa-circle");
-      selectTaskButton.id = "circle_task";
-      selectTaskButton.addEventListener("click", () => {
-        toggleOpacity(selectTaskButton, 0);
-        toggleDisplay(checkTaskButton, "flex");
-        toggleOpacity(checkTaskButton, 1);
-        input.classList.toggle("check");
-        toggleTodoItem(index);
-        // Atualiza o contador de item restantes
-        const itemsLeft = document.getElementById("items-left");
-        const activeTodos = todos.filter((todo) => !todo.isCompleted);
-        itemsLeft.textContent = activeTodos.length;
-      });
-
-      const checkTaskButton = document.createElement("i");
-      checkTaskButton.classList.add("fa-regular", "fa-circle-check");
-      checkTaskButton.id = "circle_check_task";
-      checkTaskButton.addEventListener("click", () => {
-        toggleOpacity(selectTaskButton, 1);
-        toggleDisplay(checkTaskButton, "none");
-        toggleOpacity(checkTaskButton, 0);
-        input.classList.toggle("check");
-        toggleTodoItem(index);
-        // Atualiza o contador de item restantes
-        const itemsLeft = document.getElementById("items-left");
-        const activeTodos = todos.filter((todo) => !todo.isCompleted);
-        itemsLeft.textContent = activeTodos.length;
-      });
-
-      const editDeleteButton = document.createElement("button");
-      editDeleteButton.classList.add("task_button", "edit_delete_button");
-
-      const editTaskButton = document.createElement("i");
-      editTaskButton.classList.add("fa-regular", "fa-pen-to-square");
-      editTaskButton.id = "edit_task";
-      editTaskButton.addEventListener("click", () => {
-        toggleOpacity(editTaskButton, 0);
-        toggleDisplay(SaveTaskButton, "flex");
-        toggleOpacity(SaveTaskButton, 1);
-        input.removeAttribute("disabled");
-        input.focus();
-      });
-
-      const SaveTaskButton = document.createElement("i");
-      SaveTaskButton.classList.add("fa-solid", "fa-check");
-      SaveTaskButton.id = "save_task";
-      SaveTaskButton.addEventListener("click", () => {
-        const index = input.getAttribute("data-index");
-        editTodoItem(index);
-        toggleOpacity(SaveTaskButton, 0);
-        toggleOpacity(editTaskButton, 1);
-        toggleDisplay(SaveTaskButton, "none");
-        input.setAttribute("disabled", "disabled");
-      });
-
-      const deleteTaskButton = document.createElement("i");
-      deleteTaskButton.classList.add("fa-regular", "fa-trash-can");
-      deleteTaskButton.id = "delete_task";
-      deleteTaskButton.addEventListener("click", () => {
-        removeTodoItem(index);
-      });
-
-      // Montando a estrutura do HTML
-      checkButton.appendChild(selectTaskButton);
-      checkButton.appendChild(checkTaskButton);
-
-      editDeleteButton.appendChild(editTaskButton);
-      editDeleteButton.appendChild(deleteTaskButton);
-      editDeleteButton.appendChild(SaveTaskButton);
-
-      taskContent.appendChild(input);
-
-      actionsTasks.appendChild(checkButton);
-      actionsTasks.appendChild(editDeleteButton);
-
-      todoItem.appendChild(taskContent);
-      todoItem.appendChild(actionsTasks);
-
+      const todoItem = renderTodoItem(todo, index);
       todosContainer.appendChild(todoItem);
 
-      if (todo.isCompleted) {
-        input.classList.toggle("check");
-        toggleOpacity(selectTaskButton, 0);
-        toggleDisplay(checkTaskButton, "flex");
-        toggleOpacity(checkTaskButton, 1);
-      }
-
-      // Arrastar e soltar para reordenar tarefas
+      // Funcionalidade de arrastar e soltar
       if (window.matchMedia("(min-width: 768px)").matches) {
         todoItem.setAttribute("draggable", "true");
 
@@ -177,39 +162,55 @@ document.addEventListener("DOMContentLoaded", function () {
           const lists = document.querySelectorAll(".list_tasks:not(.dragging)");
           lists.forEach((item) => item.classList.remove("not-dragging"));
 
-          // Realiza a reordenação da lista
           if (sourceIndex !== destinationIndex) {
             const temp = todos[sourceIndex];
             todos[sourceIndex] = todos[destinationIndex];
             todos[destinationIndex] = temp;
-
             renderTodos();
           }
         });
       }
     });
-    // Atualiza o contador de item restantes
-    const itemsLeft = document.getElementById("items-left");
-    const activeTodos = todos.filter((todo) => !todo.isCompleted);
-    itemsLeft.textContent = activeTodos.length;
-  }
+    updateItemCount();
+  };
 
-  // Função para lidar com o evento de adicionar tarefa
-  function handleAddTask() {
+  const addTodoItem = (item) => {
+    const todo = new Todo(item);
+    todos.push(todo);
+    renderTodos();
+  };
+
+  const removeTodoItem = (index) => {
+    todos.splice(index, 1);
+    renderTodos();
+  };
+
+  const editTodoItem = (index) => {
+    const input = getItemInput(index);
+    const newValue = input.value;
+    todos[index].item = newValue;
+    renderTodos();
+  };
+
+  const toggleTodoItem = (index) => {
+    todos[index].isCompleted = !todos[index].isCompleted;
+  };
+
+  // Lida com o evento de adicionar tarefas
+  const handleAddTask = () => {
     const input = document.getElementById("add_task_input");
     const newItem = input.value.trim();
     if (newItem !== "") {
       addTodoItem(newItem);
       input.value = "";
     }
-  }
+  };
 
-  // Evento para adicionar tarefa quando o botão for clicado
+  // Eventos
   document
     .getElementById("add_task_button")
     .addEventListener("click", handleAddTask);
 
-  // Adiciona a tarefa quando a tecla Enter for pressionada no input
   document
     .getElementById("add_task_input")
     .addEventListener("keydown", (event) => {
@@ -218,7 +219,7 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
 
-  // Eventos para filtrar as tarefas
+  // Filtrar tarefas
   document.getElementById("filter_all").addEventListener("click", () => {
     renderTodos();
   });
@@ -233,128 +234,19 @@ document.addEventListener("DOMContentLoaded", function () {
     renderFilteredTodos(completedTodos);
   });
 
-  // Função para renderizar tarefas filtradas
+  // Renderiza tarefas filtradas
   function renderFilteredTodos(filteredTodos) {
     const todosContainer = document.querySelector(".todos");
     todosContainer.innerHTML = "";
 
     filteredTodos.forEach((todo, index) => {
-      const todoItem = document.createElement("li");
-      todoItem.classList.add("card", "list_tasks");
-
-      const taskContent = document.createElement("div");
-      taskContent.classList.add("task_content");
-
-      const input = document.createElement("input");
-      input.setAttribute("type", "text");
-      input.setAttribute("disabled", "disabled");
-      input.classList.add("task_input");
-      input.setAttribute("name", "name");
-      input.setAttribute("autocomplete", "none");
-      input.setAttribute("data-index", index);
-      input.value = todo.item;
-
-      const actionsTasks = document.createElement("div");
-      actionsTasks.classList.add("actions_tasks");
-
-      const checkButton = document.createElement("button");
-      checkButton.classList.add("task_button", "button_check");
-
-      const selectTaskButton = document.createElement("i");
-      selectTaskButton.classList.add("fa-regular", "fa-circle");
-      selectTaskButton.id = "circle_task";
-      selectTaskButton.addEventListener("click", () => {
-        toggleOpacity(selectTaskButton, 0);
-        toggleDisplay(checkTaskButton, "flex");
-        toggleOpacity(checkTaskButton, 1);
-        input.classList.toggle("check");
-        toggleTodoItem(index);
-        // Atualiza o contador de item restantes
-        const itemsLeft = document.getElementById("items-left");
-        const activeTodos = todos.filter((todo) => !todo.isCompleted);
-        itemsLeft.textContent = activeTodos.length;
-      });
-
-      const checkTaskButton = document.createElement("i");
-      checkTaskButton.classList.add("fa-regular", "fa-circle-check");
-      checkTaskButton.id = "circle_check_task";
-      checkTaskButton.addEventListener("click", () => {
-        toggleOpacity(selectTaskButton, 1);
-        toggleDisplay(checkTaskButton, "none");
-        toggleOpacity(checkTaskButton, 0);
-        input.classList.toggle("check");
-        toggleTodoItem(index);
-        // Atualiza o contador de item restantes
-        const itemsLeft = document.getElementById("items-left");
-        const activeTodos = todos.filter((todo) => !todo.isCompleted);
-        itemsLeft.textContent = activeTodos.length;
-      });
-
-      const editDeleteButton = document.createElement("button");
-      editDeleteButton.classList.add("task_button", "edit_delete_button");
-
-      const editTaskButton = document.createElement("i");
-      editTaskButton.classList.add("fa-regular", "fa-pen-to-square");
-      editTaskButton.id = "edit_task";
-      editTaskButton.addEventListener("click", () => {
-        toggleOpacity(editTaskButton, 0);
-        toggleDisplay(SaveTaskButton, "flex");
-        toggleOpacity(SaveTaskButton, 1);
-        input.removeAttribute("disabled");
-        input.focus();
-      });
-
-      const SaveTaskButton = document.createElement("i");
-      SaveTaskButton.classList.add("fa-solid", "fa-check");
-      SaveTaskButton.id = "save_task";
-      SaveTaskButton.addEventListener("click", () => {
-        const index = input.getAttribute("data-index");
-        editTodoItem(index);
-        toggleOpacity(SaveTaskButton, 0);
-        toggleOpacity(editTaskButton, 1);
-        toggleDisplay(SaveTaskButton, "none");
-        input.setAttribute("disabled", "disabled");
-      });
-
-      const deleteTaskButton = document.createElement("i");
-      deleteTaskButton.classList.add("fa-regular", "fa-trash-can");
-      deleteTaskButton.id = "delete_task";
-      deleteTaskButton.addEventListener("click", () => {
-        removeTodoItem(index);
-      });
-
-      // Montando a estrutura do HTML
-      checkButton.appendChild(selectTaskButton);
-      checkButton.appendChild(checkTaskButton);
-
-      editDeleteButton.appendChild(editTaskButton);
-      editDeleteButton.appendChild(deleteTaskButton);
-      editDeleteButton.appendChild(SaveTaskButton);
-
-      taskContent.appendChild(input);
-
-      actionsTasks.appendChild(checkButton);
-      actionsTasks.appendChild(editDeleteButton);
-
-      todoItem.appendChild(taskContent);
-      todoItem.appendChild(actionsTasks);
-
+      const todoItem = renderTodoItem(todo, index);
       todosContainer.appendChild(todoItem);
-
-      if (todo.isCompleted) {
-        input.classList.toggle("check");
-        toggleOpacity(selectTaskButton, 0);
-        toggleDisplay(checkTaskButton, "flex");
-        toggleOpacity(checkTaskButton, 1);
-      }
     });
-    // Atualiza o contador de item restantes
-    const itemsLeft = document.getElementById("items-left");
-    const activeTodos = todos.filter((todo) => !todo.isCompleted);
-    itemsLeft.textContent = activeTodos.length;
+    updateItemCount();
   }
 
-  // Evento para limpar tarefas concluídas
+  // Limpa tarefas concluídas
   document.getElementById("clear_completed").addEventListener("click", () => {
     const completedIndexes = todos.reduce((indexes, todo, index) => {
       if (todo.isCompleted) {
